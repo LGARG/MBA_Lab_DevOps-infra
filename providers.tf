57ac7a8d8f09638abbecba8113717b4dae502660
LGARG <l.argentino@gmail.com>
2022-08-27 13:52:11 -0300

    nocreden

--- providers.tf
+++ providers.tf
@@ -10,7 +10,7 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/l_argentino/gcpkey.json")
+  
 
   project = "labdevopscloud-argentino"
   region  = "us-west1"
